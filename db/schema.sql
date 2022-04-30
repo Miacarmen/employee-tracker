1c4c1bb86c5befddf206f06f890d8603db8694dd
miacarmen <maitreyacarmen@gmail.com>
2022-04-29 20:44:47 -0700

    created inquire prompt questions and pseudocode for when/then

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employees (
+CREATE TABLE employee_data (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -20,11 +20,14 @@ CREATE TABLE roles (
 
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
-  
+  department_name VARCHAR(30) NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES roles(department_id)
+  ON DELETE SET NULL
 );
 
 
+-- * Bonus features to add
 -- update employee managers
 
 -- view employees by manager
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Dependencies
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
-const express = require('express');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 // Middleware
@@ -11,76 +11,80 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'employee_db',
-    },
-    console.log('Connected to the employee database')
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "employee_db",
+  },
+  console.log("Connected to the employee database")
 );
-
 
 // Routes
 
-
-
-
 // Prompt questions
-inquirer
-.prompt(
-    {
-    type: "list",
-    name: "start",
-    message: "What would you like to do?",
-    choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
-    // 
-},
+inquirer.prompt({
+  type: "list",
+  name: "start",
+  message: "What would you like to do?",
+  choices: [
+    "View All Departments",
+    "View All Roles",
+    "View All Employees",
+    "Add a Department",
+    "Add a Role",
+    "Add an Employee",
+    "Updated an Employee Role",
+  ],
+  when: (answers) => {
+    // when view all departments is selected
+    if (answers.choices === "View All Departments") {
+      // then display the departments table
+    }
+    // when view all roles is selected
+    else if (answers.choices === "View All Roles") {
+      // then display the roles table
+    } 
+    // when view all employees is selected
+    else if (answers.choices === "View All Employees") {
+        // then display the employee_data table
+    } 
+    // when add a department is selected
+    else if (answers.choices === "Add a Department") {
+        // then prompt to enter name of department
+        inquirer.prompt({
+            type: "input",
+            name: "department",
+            message: "What is the name of the department?"
+        }).then((answer) =>{
+            // then add to the database
+            console.log('Added Service to the Database');
+        });
+        
+    }
+    // when add a role is selected
+    else if(answers.choices === "Add a Role") {
+        // then prompt to enter the name, salary, and department for the role
+        // then add to the database
+    }
+    // when add an employee is selected
+    else if(answers.choices === "Add an Employee") {
+        // then prompt to enter first_name, last_name, role, and manager
+        // then add employee to database
+    }
+    // when update an employee role is selected
+    else if(answers.choices === "Update an Employee Role") {
+        // then prompt to select an employee to update
+        // then add updated employee_data in the database
+    }
+  }
+  
+})
+.then((answers) => {
 
-{
-    type: "",
-    name: "",
-    message: "",
-    choices: []
-},
-
-{
-    type: "",
-    name: "",
-    message: "",
-    choices: []
-},
-
-{
-    type: "",
-    name: "",
-    message: "",
-    choices: []
-},
-
-{
-    type: "",
-    name: "",
-    message: "",
-    choices: []
-},
-
-{
-    type: "",
-    name: "",
-    message: "",
-    choices: []
-},
-
-{
-    type: "",
-    name: "",
-    message: "",
-    choices: []
-},
+});
 
 
-);
